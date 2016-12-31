333f2d15d19d7b0c2d8fe352963b0f27f1d3cb87
SylvanasSun <sylvanassun_xtz@163.com>
2016-12-31 11:38:07 +0800

    UPDATE JDBCUtils.

--- src/main/java/com/sun/sylvanas/utils/JDBCUtils.java
+++ src/main/java/com/sun/sylvanas/utils/JDBCUtils.java
@@ -1,7 +1,11 @@
 package com.sun.sylvanas.utils;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.apache.log4j.Logger;
 
+import javax.sql.rowset.CachedRowSet;
+import javax.sql.rowset.RowSetProvider;
+import java.beans.PropertyVetoException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
@@ -21,6 +25,7 @@ public class JDBCUtils {
     private final static String URL;
     private final static String USERNAME;
     private final static String PASSWORD;
+    private static ComboPooledDataSource dataSource = null;
     private static Connection connection = null;
     private static PreparedStatement statement = null;
     private static ResultSet resultSet = null;
@@ -35,7 +40,7 @@ public class JDBCUtils {
             properties.load(new FileInputStream("db.properties"));
         } catch (IOException e) {
             e.printStackTrace();
-            logger.error("Error: " + JDBCUtils.class.getName() + " class initializer fail.", e);
+            logger.error("Error: " + JDBCUtils.class.getName() + " properties initializer fail.", e);
         }
         DRIVER = properties.getProperty("driver");
         URL = properties.getProperty("url");
@@ -43,26 +48,38 @@ public class JDBCUtils {
         PASSWORD = properties.getProperty("password");
 
         try {
-            Class.forName(DRIVER);
-        } catch (ClassNotFoundException e) {
+            //初始化c3p0连接池
+            dataSource = new ComboPooledDataSource();
+            dataSource.setDriverClass(DRIVER);
+            dataSource.setJdbcUrl(URL);
+            dataSource.setUser(USERNAME);
+            dataSource.setPassword(PASSWORD);
+            dataSource.setMaxPoolSize(40);
+            dataSource.setMinPoolSize(2);
+            dataSource.setInitialPoolSize(5);
+            dataSource.setMaxStatements(180);
+        } catch (PropertyVetoException e) {
             e.printStackTrace();
-            logger.error("Error: " + JDBCUtils.class.getName() + " load driver fail.");
+            logger.error("Error: " + JDBCUtils.class.getName() + " dataSource initializer fail.");
         }
     }
 
     /**
      * 对应JDBC API中的execute方法,发生异常返回-1.
      *
-     * @param sql   sql语句
-     * @param param 参数
-     * @return 如果execute为true(Query操作)返回结果集, 否则为所影响的行数(DML操作).
+     * @param sql      sql语句
+     * @param pageSize 每页个数
+     * @param page     要查询的页
+     * @param param    参数
+     * @return 如果execute为true(Query操作)返回离线的分页RowSet, 否则为所影响的行数(DML操作).
      */
-    public static Object execute(String sql, Object... param) {
+    public static Object execute(String sql, Integer pageSize, Integer page, Object... param) {
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false); //开启事务
             statement = connection.prepareStatement(sql);
             //设置参数
-            if (param != null && param.length >= 0) {
+            if (param != null && param.length > 0) {
                 final AtomicInteger count = new AtomicInteger(1);
                 Arrays.stream(param).forEach((i) -> {
                     try {
@@ -70,32 +87,57 @@ public class JDBCUtils {
                     } catch (SQLException e) {
                         e.printStackTrace();
                         logger.error("Error: " + JDBCUtils.class.getName() + " set sql param fail.", e);
+                        try {
+                            connection.rollback();
+                        } catch (SQLException e1) {
+                            e1.printStackTrace();
+                            logger.error("Error: " + JDBCUtils.class.getName() + " transaction rollback fail.", e1);
+                        }
                     }
                 });
             }
-            //执行
+            //执行sql
             boolean execute = statement.execute();
+            connection.commit(); //提交事务
             if (execute) {
-                //true,返回ResultSet
+                //创建离线的RowSet
+                CachedRowSet cachedRowSet = RowSetProvider.newFactory().createCachedRowSet();
+                //true,包装并返回RowSet
                 resultSet = statement.getResultSet();
-                if (resultSet != null)
-                    return resultSet;
-                else
+                if (resultSet != null) {
+                    //判断是否使用分页
+                    if (page != null && pageSize != null) {
+                        cachedRowSet.populate(resultSet, (page - 1) * pageSize + 1);
+                    } else {
+                        cachedRowSet.populate(resultSet);
+                    }
+                    return cachedRowSet;
+                } else {
                     return -1;
+                }
             } else {
                 return statement.getUpdateCount();
             }
         } catch (SQLException e) {
             e.printStackTrace();
             logger.error("Error: " + JDBCUtils.class.getName() + " sql fail.", e);
+            try {
+                connection.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+                logger.error("Error: " + JDBCUtils.class.getName() + " transaction rollback fail.", e1);
+            }
         } finally {
             try {
-                if (resultSet != null)
+                if (resultSet != null) {
                     resultSet.close();
-                if (statement != null)
+                }
+                if (statement != null) {
                     statement.close();
-                if (connection != null)
+                }
+                if (connection != null) {
                     connection.close();
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
                 logger.error("Error: " + JDBCUtils.class.getName() + " close resource fail.", e);
@@ -103,4 +145,17 @@ public class JDBCUtils {
         }
         return -1;
     }
+
+    /**
+     * 对应JDBC API中的execute方法,发生异常返回-1.
+     *
+     * @param sql   sql语句
+     * @param param 参数
+     * @return 如果execute为true(Query操作)返回离线RowSet, 否则为所影响的行数(DML操作).
+     */
+    public static Object execute(String sql, Object... param) {
+        return JDBCUtils.execute(sql, null, null, param);
+    }
+
+
 }
--- src/test/java/com/sun/sylvanas/pattern/test/CommonTest.java
+++ src/test/java/com/sun/sylvanas/pattern/test/CommonTest.java
@@ -2,6 +2,7 @@ package com.sun.sylvanas.pattern.test;
 
 import org.junit.Test;
 
+import java.sql.*;
 import java.util.Arrays;
 
 /**
@@ -12,5 +13,32 @@ public class CommonTest {
     public void test01() {
         String[] strings = {"hello", "world", "atomicity", "consistency", "isolate", "durability"};
         Arrays.stream(strings).forEach((i) -> System.out.println(i));
+    }
+
+    @Test
+    public void test02() throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/test";
+        String user = "root";
+        String pass = "root";
+
+        Class.forName(driver);
+        Connection connection = DriverManager.getConnection(url, user, pass);
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet test_student = metaData.getColumns(null, null, "test_student", "%");
+
+        ResultSetMetaData rsmd = test_student.getMetaData();
+        for (int i = 0; i < rsmd.getColumnCount(); i++) {
+            System.out.print(rsmd.getColumnName(i + 1) + "\t");
+        }
+        System.out.print("\n");
+        while (test_student.next()) {
+            for (int i = 0; i < rsmd.getColumnCount(); i++) {
+                System.out.print(test_student.getString(i + 1) + "\t");
+            }
+            System.out.print("\n");
+        }
+        test_student.close();
+        connection.close();
     }
 }
