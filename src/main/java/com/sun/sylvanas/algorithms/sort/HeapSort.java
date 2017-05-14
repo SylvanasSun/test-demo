0dc1b608686d5f8a255a5b7c6f099613105cc8d5
SylvanasSun <sylvanassun_xtz@163.com>
2017-05-14 15:13:59 +0800

    add heap sort.

---
+++ src/main/java/com/sun/sylvanas/algorithms/sort/HeapSort.java
@@ -0,0 +1,48 @@
+package com.sun.sylvanas.algorithms.sort;
+
+import com.sun.sylvanas.data_struct.MaxHeap;
+
+import java.util.Scanner;
+
+/**
+ * Created by SylvanasSun on 2017/5/14.
+ */
+public class HeapSort {
+
+    private static boolean priorityQueueFlag = false; // 是否使用优先队列
+
+    public static void sort(Comparable[] a) {
+        if (!priorityQueueFlag) {
+            maxHeapSort(a);
+        }
+    }
+
+    // 使用最大堆实现堆排序
+    private static void maxHeapSort(Comparable[] a) {
+        MaxHeap<Comparable> maxHeap = new MaxHeap<>(a);
+        print(a);
+        //不断地将最大堆中顶端元素(最大值)与最底部的元素(最小值)交换
+        for (int i = a.length - 1; i > 0; i--) {
+            Comparable largest = a[0];
+            a[0] = a[i];
+            a[i] = largest;
+            // 堆减少,并调整新的堆
+            maxHeap.heapify(a, i, 0);
+        }
+    }
+
+    public static void print(Object[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please input test data.");
+        String[] s = new Scanner(System.in).nextLine().split("\\s+");
+        HeapSort.sort(s);
+        HeapSort.print(s);
+    }
+
+}
---
+++ src/main/java/com/sun/sylvanas/data_struct/MaxHeap.java
@@ -0,0 +1,71 @@
+package com.sun.sylvanas.data_struct;
+
+/**
+ * 最大堆的简单实现
+ * <p>
+ * Created by SylvanasSun on 2017/5/14.
+ */
+public class MaxHeap<T extends Comparable> {
+
+    T[] heap;
+
+    private MaxHeap() {
+    }
+
+    public MaxHeap(T[] heap) {
+        this.heap = heap;
+        buildHeap();
+    }
+
+    /**
+     * 自底向上构建堆
+     */
+    private void buildHeap() {
+        int length = heap.length;
+        // 当堆为空或者长度为1时不需要任何操作
+        if (length <= 1)
+            return;
+
+        int root = (length - 2) >>> 1; // (i - 1) / 2
+        while (root >= 0) {
+            heapify(heap, length, root);
+            root--;
+        }
+    }
+
+    /**
+     * 调整堆的结构
+     *
+     * @param heap   堆
+     * @param length 堆的长度
+     * @param root   根节点索引
+     */
+    public void heapify(T[] heap, int length, int root) {
+        if (root >= length)
+            return;
+
+        int largest = root; // 表示root,left,right中最大值的变量
+        int left = (root << 1) + 1; // 左子节点,root * 2 + 1
+        int right = left + 1; // 右子节点,root * 2 + 2
+
+        // 找出最大值
+        if (left < length && greater(heap[left], heap[largest]))
+            largest = left;
+        if (right < length && greater(heap[right], heap[largest]))
+            largest = right;
+
+        // 如果largest发生变化,将largest与root交换
+        if (largest != root) {
+            T t = heap[root];
+            heap[root] = heap[largest];
+            heap[largest] = t;
+            // 继续向下调整堆
+            heapify(heap, length, largest);
+        }
+    }
+
+    private boolean greater(Comparable a, Comparable b) {
+        return a.compareTo(b) > 0;
+    }
+
+}
