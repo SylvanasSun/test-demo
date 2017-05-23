6be1e0af319d134150aee4720e87c77cdfe72c42
SylvanasSun <sylvanassun_xtz@163.com>
2017-05-23 21:28:06 +0800

    add binary tree.

---
+++ src/main/java/com/sun/sylvanas/data_struct/tree/BinaryTree.java
@@ -0,0 +1,114 @@
+package com.sun.sylvanas.data_struct.tree;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+/**
+ * The {@code BinaryTree} class represents an ordered symbol table of generic
+ * key-value pairs.
+ * This implementation uses an (unbalanced) binary search tree.
+ * It requires that the key type implements the {@code Comparable} interface and
+ * calls the {@code compareTo()} and method to compare two keys.
+ * It does not call either {@code equals()} or {@code hashCode()}.
+ * <p>
+ * Created by SylvanasSun on 2017/5/23.
+ */
+public class BinaryTree<K extends Comparable<K>, V> implements Serializable, Iterable<K> {
+
+    private static final long serialVersionUID = -712550370847248659L;
+
+    private Node<K, V> root;
+
+    public BinaryTree() {
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     *
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return size(root);
+    }
+
+    protected int size(Node<K, V> x) {
+        return (x == null) ? 0 : x.getSize();
+    }
+
+    /**
+     * This symbol table is empty?
+     *
+     * @return {@code true} if this symbol table table is empty and {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    /**
+     * Returns the value associated with the given key.
+     *
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     * and {@code null} if  the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public V get(K key) {
+        if (key == null)
+            throw new IllegalArgumentException();
+        return get(root, key);
+    }
+
+    protected V get(Node<K, V> x, K key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.getKey());
+            if (cmp < 0)
+                x = x.getLeft();
+            else if (cmp > 0)
+                x = x.getRight();
+            else
+                return x.getValue();
+        }
+        return null;
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     *
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key}
+     * and {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(K key) {
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the successor of the node x or null if no such.
+     * successor is the right subtree the leftmost node.
+     */
+    protected Node<K, V> successor(Node<K, V> x) {
+        if (x == null)
+            return null;
+        Node<K, V> right = x.getRight();
+        if (right != null) {
+            while (right.getLeft() != null)
+                right = right.getLeft();
+            return right;
+        } else {
+            Node<K, V> t = x;
+            Node<K, V> p = x.getParent();
+            while (p != null && t == p.getRight()) {
+                t = p;
+                p = p.getParent();
+            }
+            return p;
+        }
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/sun/sylvanas/data_struct/tree/Node.java
@@ -0,0 +1,108 @@
+package com.sun.sylvanas.data_struct.tree;
+
+/**
+ * Created by SylvanasSun on 2017/5/23.
+ */
+public class Node<K, V> {
+
+    private K key;
+    private V value;
+    private int size = 0;
+    private Node<K, V> parent, left, right;
+
+    public Node(K key, V value, int size, Node<K, V> parent, Node<K, V> left, Node<K, V> right) {
+        this.key = key;
+        this.value = value;
+        this.size = size;
+        this.parent = parent;
+        this.left = left;
+        this.right = right;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Node<K, V> getParent() {
+        return parent;
+    }
+
+    public void setParent(Node<K, V> parent) {
+        this.parent = parent;
+    }
+
+    public Node<K, V> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<K, V> left) {
+        this.left = left;
+    }
+
+    public Node<K, V> getRight() {
+        return right;
+    }
+
+    public void setRight(Node<K, V> right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "key=" + key +
+                ", value=" + value +
+                ", size=" + size +
+                ", parent=" + parent +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node<?, ?> node = (Node<?, ?>) o;
+
+        if (size != node.size) return false;
+        if (key != null ? !key.equals(node.key) : node.key != null) return false;
+        if (value != null ? !value.equals(node.value) : node.value != null) return false;
+        if (parent != null ? !parent.equals(node.parent) : node.parent != null) return false;
+        if (left != null ? !left.equals(node.left) : node.left != null) return false;
+        return right != null ? right.equals(node.right) : node.right == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key != null ? key.hashCode() : 0;
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        result = 31 * result + size;
+        result = 31 * result + (parent != null ? parent.hashCode() : 0);
+        result = 31 * result + (left != null ? left.hashCode() : 0);
+        result = 31 * result + (right != null ? right.hashCode() : 0);
+        return result;
+    }
+
+}
