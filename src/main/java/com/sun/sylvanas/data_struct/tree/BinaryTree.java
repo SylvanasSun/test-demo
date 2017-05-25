6e3faf07c847b7986d86fbf5063fe309f9cb3cf2
SylvanasSun <sylvanassun_xtz@163.com>
2017-05-25 23:07:48 +0800

    update binary tree.

--- src/main/java/com/sun/sylvanas/data_struct/tree/BinaryTree.java
+++ src/main/java/com/sun/sylvanas/data_struct/tree/BinaryTree.java
@@ -1,8 +1,10 @@
 package com.sun.sylvanas.data_struct.tree;
 
 import java.io.Serializable;
+import java.util.ArrayDeque;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 
 /**
  * The {@code BinaryTree} class represents an ordered symbol table of generic
@@ -175,6 +177,253 @@ public class BinaryTree<K extends Comparable<K>, V> implements Serializable, Ite
         return get(key) != null;
     }
 
+    /**
+     * Returns the smallest key in the symbol table.
+     *
+     * @return the smallest key in the symbol table
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public K min() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        Node<K, V> x = root;
+        while (x.getLeft() != null)
+            x = x.getLeft();
+        return x.getKey();
+    }
+
+    /**
+     * Returns the largest key in the symbol table.
+     *
+     * @return the largest key in the symbol table
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public K max() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        Node<K, V> x = root;
+        while (x.getRight() != null)
+            x = x.getRight();
+        return x.getKey();
+    }
+
+    /**
+     * Removes the smallest key and associated value from the symbol table.
+     * and return old value.
+     *
+     * @return @return the old value (if return {@code null} symbol table no contain the key)
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public V removeMin() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        V oldValue = get(min());
+        if (oldValue == null)
+            return null;
+        removeMin(root);
+        return oldValue;
+    }
+
+    protected void removeMin(Node<K, V> x) {
+        while (x != null) {
+            if (x.getLeft() != null)
+                x = x.getLeft();
+            else {
+                Node<K, V> successor = x.getRight();
+                removeSingleNode(x, successor);
+                x = null;
+            }
+        }
+    }
+
+    /**
+     * Removes the largest key and associated value from the symbol table.
+     * and return old value.
+     *
+     * @return @return the old value (if return {@code null} symbol table no contain the key)
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public V removeMax() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        V oldValue = get(max());
+        if (oldValue == null)
+            return null;
+        removeMax(root);
+        return oldValue;
+    }
+
+    protected void removeMax(Node<K, V> x) {
+        while (x != null) {
+            if (x.getRight() != null)
+                x = x.getRight();
+            else {
+                Node<K, V> successor = x.getLeft();
+                removeSingleNode(x, successor);
+                x = null;
+            }
+        }
+    }
+
+    /**
+     * Returns the largest key in the symbol table less than or equals to {@code key}.
+     *
+     * @param key the key
+     * @return the largest key in the symbol table less than or equals to {@code key}
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * @throws NoSuchElementException   if there is no such key
+     */
+    public K floor(K key) {
+        if (key == null)
+            throw new IllegalArgumentException();
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        Node<K, V> x = floor(root, key);
+        if (x == null)
+            return null;
+        else
+            return x.getKey();
+    }
+
+    protected Node<K, V> floor(Node<K, V> x, K key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.getKey());
+            if (cmp > 0) {
+                if (x.getRight() != null)
+                    x = x.getRight();
+                else
+                    return x;
+            } else if (cmp < 0) {
+                if (x.getLeft() != null)
+                    x = x.getLeft();
+                else {
+                    Node<K, V> p = x.getParent();
+                    Node<K, V> t = x;
+                    while (p != null && t == p.getLeft()) {
+                        t = p;
+                        p = p.getParent();
+                    }
+                    return p;
+                }
+            } else {
+                return x;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns the smallest key in the symbol table greater than or equals to {@code key}.
+     *
+     * @param key the key
+     * @return the smallest key in the symbol table greater than or equals to {@code key}
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * @throws NoSuchElementException   if there is no such key
+     */
+    public K ceiling(K key) {
+        if (key == null)
+            throw new IllegalArgumentException();
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        Node<K, V> x = ceiling(root, key);
+        if (x == null)
+            return null;
+        else
+            return x.getKey();
+    }
+
+    protected Node<K, V> ceiling(Node<K, V> x, K key) {
+        while (x != null) {
+            int cmp = key.compareTo(x.getKey());
+            if (cmp < 0) {
+                if (x.getLeft() != null)
+                    x = x.getLeft();
+                else
+                    return x;
+            } else if (cmp > 0) {
+                if (x.getRight() != null)
+                    x = x.getRight();
+                else {
+                    Node<K, V> t = x;
+                    Node<K, V> p = x.getParent();
+                    while (p != null && t == x.getRight()) {
+                        t = p;
+                        p = p.getParent();
+                    }
+                    return p;
+                }
+            } else {
+                return x;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Return the kth smallest key in the symbol table.
+     *
+     * @param k the order statistic
+     * @return the {@code k}th smallest key in the symbol table
+     * @throws IllegalArgumentException unless {@code k} is between 0 and <em>n</em> - 1
+     */
+    public K select(int k) {
+        if (k < 0 || k >= size())
+            throw new IllegalArgumentException();
+
+        Node<K, V> x = select(root, k);
+        if (x == null)
+            return null;
+        else
+            return x.getKey();
+    }
+
+    protected Node<K, V> select(Node<K, V> x, int k) {
+        while (x != null) {
+            int t = size(x.getLeft());
+            if (t > k)
+                x = x.getLeft();
+            else if (t < k) {
+                x = x.getRight();
+                k = k - t - 1;
+            } else {
+                return x;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Return the number of keys in the symbol table strictly less than {@code key}.
+     *
+     * @param key the key
+     * @return the number of keys in the symbol table strictly less than {@code key}
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public int rank(K key) {
+        if (key == null)
+            throw new IllegalArgumentException();
+
+        return rank(root, key);
+    }
+
+    protected int rank(Node<K, V> x, K key) {
+        if (x == null)
+            return 0;
+        int cmp = key.compareTo(x.getKey());
+        if (cmp < 0)
+            return rank(x.getLeft(), key);
+        else if (cmp > 0)
+            return 1 + size(x.getLeft()) + rank(x.getRight(), key);
+        else
+            return size(x.getLeft());
+    }
+
     protected void removeSingleNode(Node<K, V> x, Node<K, V> successor) {
         if (successor != null) {
             Node<K, V> xParent = x.getParent();
@@ -227,9 +476,42 @@ public class BinaryTree<K extends Comparable<K>, V> implements Serializable, Ite
         }
     }
 
+    protected class TreeIterator implements Iterator<K> {
+
+        private Queue<K> queue;
+        private Node<K, V> root;
+
+        public TreeIterator(Node<K, V> root) {
+            this.root = root;
+            this.queue = new ArrayDeque<K>();
+            inorder(root);
+        }
+
+        protected void inorder(Node<K, V> x) {
+            if (x.getLeft() != null)
+                inorder(x.getLeft());
+            queue.add(x.getKey());
+            if (x.getRight() != null)
+                inorder(x.getRight());
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !queue.isEmpty();
+        }
+
+        @Override
+        public K next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            return queue.remove();
+        }
+
+    }
+
     @Override
     public Iterator<K> iterator() {
-        return null;
+        return new TreeIterator(root);
     }
 
 }
